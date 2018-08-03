c1ac1061299ccb7faea71eda719914ad15d92eb0
sisazac <41337764+sisazac@users.noreply.github.com>
2018-08-03 09:42:41 -0500

    Add files via upload

--- laboratorios/lab01/ejercicioEnLinea/CodingBat/Recursion2.java
+++ laboratorios/lab01/ejercicioEnLinea/CodingBat/Recursion2.java
@@ -1,17 +1,44 @@
 
 /**
+
  * Recursion2
+
  * Ejercicios de recursión 2 de codingbat
+
  * @author Santiago  Isaza Cadavid y Hamilton Smith Gómez Osorio
+
  * @version Agosto-2018
+
  */
+
 public class Recursion2
+
 {
+
     public boolean groupSum6(int start, int[] nums, int target) {
         if(start>=nums.length) return target==0;
         if(nums[start]==6) return groupSum6(start+1,nums,target-nums[start]);
         return groupSum6(start+1,nums,target-nums[start]) || 
         groupSum6(start+1,nums,target);
     }
+
+    public boolean groupNoAdj(int start, int[] nums, int target) {
+        if(start>=nums.length) return target==0;
+        return groupNoAdj(start+1,nums,target) ||
+        groupNoAdj(start+2,nums,target-nums[start]);
+    }
+
+    public boolean groupSum5(int start, int[] nums, int target) {
+        if(start>=nums.length) return target==0;
+        if (nums[start]%5==0){
+            if(start<nums.length-1 && nums[start+1]==1){
+                return groupSum5(start+2,nums,target-nums[start]);
+            }
+            return groupSum5(start+1,nums,target-nums[start]);
+        }
+        return groupSum5(start+1,nums,target) ||
+        groupSum5(start+1,nums,target-nums[start]);
+    }
+
 
 }

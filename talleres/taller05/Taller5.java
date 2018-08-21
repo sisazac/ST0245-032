3a507ed40b2d65e1cfb71e0d7814d10f70b7d60b
sisazac <41337764+sisazac@users.noreply.github.com>
2018-08-21 13:00:47 -0500

    Add files via upload

--- talleres/taller05/Taller5.java
+++ talleres/taller05/Taller5.java
@@ -12,7 +12,7 @@
  */
 import java.util.concurrent.TimeUnit;
 public class Taller5{
-    
+
     public static void main(String[] args){
         for (int i = 10000000; i <= 200000000; i = i + 10000000){
             int[] a = new int[i];
@@ -33,21 +33,13 @@ public class Taller5{
         return suma; //c5
     }
 
-
     /**
-
      * @param num es el numero el cual se utiliza para ser multiplicado.
-
      * El método mul tiene la intención de hacer la multiplicación
-
      * de 1 a n por el numero mul
-
      * mediante una funcion cíclica (while/for/...)
-
      * 
-
      */
-
     public static void mul (int num){
         for(int i=1;i<=num;i++){
             for(int j=1;j<=num;j++){
@@ -57,24 +49,26 @@ public class Taller5{
     }
 
     /**
-
      * @param array es un arreglo de números desordenados
-
      * El método insertionSort tiene la intención ordenar los números
-
      * del arreglo array por el método insertion:
-
      * @see <a href="https://www.youtube.com/watch?v=OGzPmgsI-pQ"> Insertion Sort <a/>
-
      * mediante la anidación de funciones cíclicas (while/for/...)
-
      * 
-
      */
-
-    //public static int[] insertionSort (int[] array){
-
-    //...
-
-}    
+    public static int[] insertionSort (int[] array){
+        for(int a=0;a<array.length;a++){
+            int menor= a;
+            for(int b=a+1;b<array.length;b++){
+                if(array[b]< array[menor]){
+                    menor= b;
+                }
+            }
+            int aux= array[a];
+            array[a]= array[menor];
+            array[menor]= aux;
+        }
+        return array;
+    }  
+}  
 

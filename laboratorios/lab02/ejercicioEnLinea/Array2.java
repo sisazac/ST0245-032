b4172c00b69f0cca0644f8ee977efc6e87843d1a
sisazac <41337764+sisazac@users.noreply.github.com>
2018-08-27 11:49:58 -0500

    Add files via upload

--- laboratorios/lab02/ejercicioEnLinea/Array2.java
+++ laboratorios/lab02/ejercicioEnLinea/Array2.java
@@ -28,4 +28,11 @@ public class Array2
         return true;
     }
 
+    public boolean modThree(int[] nums) {
+        for(int i=0;i<nums.length-2;i++){
+            if(nums[i]%2==0 && nums[i+1]%2==0 && nums[i+2]%2==0) return true;
+            if(nums[i]%2==1 && nums[i+1]%2==1 && nums[i+2]%2==1) return true;
+        }
+        return false;
+    }
 }

41d2450dd9df56049ee6e3ff41857cce9e0b7102
sisazac <41337764+sisazac@users.noreply.github.com>
2018-08-27 13:28:21 -0500

    Add files via upload

--- laboratorios/lab02/ejercicioEnLinea/Array2.java
+++ laboratorios/lab02/ejercicioEnLinea/Array2.java
@@ -35,4 +35,11 @@ public class Array2
         }
         return false;
     }
+
+    public boolean tripleUp(int[] nums) {
+        for(int i=0;i<nums.length-2;i++){
+            if(nums[i+1]==nums[i]+1 && nums[i+2]==nums[i]+2) return true;
+        }
+        return false;
+    }
 }

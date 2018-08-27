de1e478bcde7dc99ccc7d38cba70eb37377bdf7e
sisazac <41337764+sisazac@users.noreply.github.com>
2018-08-27 11:40:04 -0500

    Add files via upload

--- laboratorios/lab02/ejercicioEnLinea/Array2.java
+++ laboratorios/lab02/ejercicioEnLinea/Array2.java
@@ -13,4 +13,12 @@ public class Array2
         }
         return n;
     }
+
+    public boolean lucky13(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==3 || nums[i]==1) return false;
+        }
+        return true;
+    }
+
 }

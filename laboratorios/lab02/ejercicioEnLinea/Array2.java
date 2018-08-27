8e831f414f0bdf2e55715f4c6c1e78964cef4e6e
sisazac <41337764+sisazac@users.noreply.github.com>
2018-08-27 11:44:41 -0500

    Add files via upload

--- laboratorios/lab02/ejercicioEnLinea/Array2.java
+++ laboratorios/lab02/ejercicioEnLinea/Array2.java
@@ -21,4 +21,11 @@ public class Array2
         return true;
     }
 
+    public boolean isEverywhere(int[] nums, int val) {
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]!=val && nums[i+1]!=val) return false;
+        }
+        return true;
+    }
+
 }

00932de81eff6bcea8cdbb9d26f87d621ef7cdec
sisazac <41337764+sisazac@users.noreply.github.com>
2018-08-28 18:33:04 -0500

    Add files via upload

--- talleres/taller06/MiArrayList.java
+++ talleres/taller06/MiArrayList.java
@@ -1,149 +1,78 @@
 import java.util.Arrays;
 
-
 /**
-
  * La clase MiArrayList tiene la intención de representar un objeto que simule el comportamiento
-
  * de la clase ya implementada "ArrayList"
-
  * es claro que no se puede utilizar dicha estructura ya utilizada dentro de este ejercicio. 
-
  * Para más información de la clase ArrayList:
-
  * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html"> Ver documentacion ArrayList </a>
-
  * 
-
  * 
-
  * @author Mauricio Toro, Andres Paez
-
  * @version 1
-
  */
-
-
 public class MiArrayList {
 
     private int size;
-
     private static final int DEFAULT_CAPACITY = 10;
-
     private int elements[]; 
-
-
     /**
-
      * El metodo constructor se utiliza para incializar
-
      * variables a valores neutros como 0 o null.
-
      * El contructor no lleva parámetros en este caso.
-
      */
-
     public MiArrayList() {
-
         elements= new int [DEFAULT_CAPACITY];
-
         size=0;
-
-
     }     
 
-
     /**
-
      * Tiene la intención de retornar la longitud del objeto
-
      * @return longitud del objeto
-
      *
-
      * El size esta influenciado por las funciones add y del
-
      */
-
     public int size() {
-
         return size;
-
     }   
 
-
     /** 
-
      * @param e el elemento a guardar
-
      * Agrega un elemento e a la última posición de la lista
-
      *
-
      */
-
     public void add(int e) {
-
         if(size<= elements.length){
-
             elements[size]= e;
-
         }else{
-
             int nuevosElements[]= new int[DEFAULT_CAPACITY*2];
-
             for(int i=0;i<size;i++){
-
                 nuevosElements[i]= elements[i];
-
             }
-
             nuevosElements[size]= e;
-
         }
-
         size++;
-
     }    
 
-
     /** 
-
      * @param i es un íncide donde se encuentra el elemento posicionado
-
      * Retorna el elemento que se encuentra en la posición i de la lista.
-
      *
-
      */
-
     public int get(int i) throws Exception{
-
         if( i>=0 && i<size ){
-
             return elements[i];
-
         }else{
-
             throw new Exception("la i no es válida");
-
         }
-
     }
 
-
     /**
-
      * @param index es la posicion en la cual se va agregar el elemento
-
      * @param e el elemento a guardar
-
      * Agrega un elemento e en la posición index de la lista
-
      *
-
      */
-
     public void add(int index, int e) {
         if (size == elements.length) {
             int[] aux = new int[elements.length * 2];
@@ -178,29 +107,16 @@ public class MiArrayList {
 
 
     /**
-
      * @param index es la posicion en la cual se va agregar el elemento
-
      *
-
      * ELimina el elemento  en la posición index de la lista
-
      *
-
      */
-
     public void del(int index){
-
         if(index>=0 && index<size){
-
             for(int i= index;i<size;i++){
-
                 elements[i]= elements[i+1];
-
             }
-
         }
-
     }
-
 }

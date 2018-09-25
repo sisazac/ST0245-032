e56e3bfb82e0873671db8cce85aa0bda377677ed
sisazac <41337764+sisazac@users.noreply.github.com>
2018-09-25 10:54:10 -0500

    Add files via upload

---
+++ laboratorios/lab03/codigo/smartInsertV2.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class smartInsertV2{
+    public static ArrayList<Integer> list;
+    public int size;
+    public smartInsertV2()
+    {
+        size = list.size();
+    }
+
+    public static void smartInsert(ArrayList<Integer> list, int data){
+        int index= list.size()-1;
+        int n=0;
+        boolean ok=true;
+        try{
+            for(int i=0;i<index;i++){
+                n = list.get(i);
+                if(data==n){
+                    System.out.println("Sorry mate that is the wrong number");
+                    ok= false;
+                    break;
+                }
+            }
+            if(ok==true){
+                list.add(data);
+                System.out.println("The last number is added is:"+ list.get(list.size()-1));
+            }
+        }catch(Exception e){
+            System.out.println("Not allowed value");
+        }
+    }
+
+    /**
+     * Tests the methods.
+     * @param args
+     */
+    public static void main(String[] args) {
+        //LinkedList<Integer> linked= new LinkedList<>();
+        //linked.addAll(Arrays.asList(new Integer[] {1, 3, 5, 7}));
+        ArrayList<Integer> array = new ArrayList<>();
+        array.addAll(Arrays.asList(new Integer[] {2, 4, 6, 8}));
+        smartInsert(array, 5);
+    }
+}

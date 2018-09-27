5f89131dcf3b4af4e08a6a5fa52c26bfc068a5ae
sisazac <41337764+sisazac@users.noreply.github.com>
2018-09-26 23:30:16 -0500

    Add files via upload

--- talleres/taller08/Taller8.java
+++ talleres/taller08/Taller8.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 
 /* Title: Polish Calculator
-     * Authosr: Lalinde, Juliana and Urrego, Isabel
+     * Authors: Lalinde, Juliana and Urrego, Isabel
      * Date: September 26, 2018
      * Taken from:https://github.com/julianalalindev/ST0245-032/blob/master/talleres/taller08/Taller8.java
      */

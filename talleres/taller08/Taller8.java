e655da9ba12b0f0c439a2c48beb27eebf3f70564
sisazac <41337764+sisazac@users.noreply.github.com>
2018-09-26 23:07:08 -0500

    Add files via upload

---
+++ talleres/taller08/Taller8.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+
+
+/**
+ * @author Juliana Lalinde and  Isabel Urrego
+ */
+
+public class Taller8 {
+        public static boolean isInteger(String s) {
+        try { 
+            Integer.parseInt(s); 
+        } catch(NumberFormatException e) { 
+            return false; 
+        } catch(NullPointerException e) {
+            return false;
+        }
+        return true;
+    }
+    
+    public static int Polish(String string){
+        String[] s= string.split(" ");
+        Stack<String> aux= new Stack<String>();
+        String temp;
+        int num=0;
+        for(int i=0; i<s.length; i++){
+            temp=s[s.length-1-i];
+            if(isInteger(temp)){
+                aux.push(temp);
+            }else{
+                if(temp.equals("+")){
+
+                    num= Integer.parseInt(aux.pop())+ Integer.parseInt(aux.pop());
+                }
+                if(temp.equals("-")){
+                    num= -(Integer.parseInt(aux.pop())- Integer.parseInt(aux.pop()));
+                }
+                if(temp.equals("*")){
+                    num= Integer.parseInt(aux.pop())* Integer.parseInt(aux.pop());
+                }
+                if(temp.equals("/")){
+                    num= Integer.parseInt(aux.pop())/ Integer.parseInt(aux.pop());
+                }
+                if(temp.equals("%")){
+                    num= Integer.parseInt(aux.pop())% Integer.parseInt(aux.pop());
+                }
+            }
+        }
+        return num;
+    }
+}

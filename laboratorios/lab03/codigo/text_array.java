e90c9b2090970f5291d18fb34c93a0f72235b852
sisazac <41337764+sisazac@users.noreply.github.com>
2018-10-07 15:58:22 -0500

    Add files via upload

---
+++ laboratorios/lab03/codigo/text_array.java
@@ -0,0 +1,38 @@
+import java.util.*;
+/**
+ *Text
+ * 
+ * @author Santiago Isaza Cadavid and Hamilton Smith Gómez Osorio 
+ * @version October 2018
+ */
+public class text_array
+{
+    public static String pc(String str){
+        List<String> list = new ArrayList<>();
+        boolean start= true;
+        int index=0;
+        String newString="";
+        for(int i=0;i<str.length();i++){
+            if(str.substring(i,i+1).equals("[")){
+                start=true;
+                index=0;
+            }
+            else if(str.substring(i,i+1).equals("]")){
+                start=false;
+            }else if(!str.substring(i,i+1).equals("[") && !str.substring(i,i+1).equals("]")){
+                if(start){
+                    list.add(index,str.substring(i,i+1));
+                    index++;
+                }else{
+                    list.add(str.substring(i,i+1));
+                }
+            }
+        }
+        
+        for(int i=0;i<list.size();i++){
+            newString= newString+list.get(i);
+        }
+        
+        return newString;
+    }
+}

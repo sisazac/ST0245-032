88cd59aad8c82ac3aced04af2abede6edda3c867
sisazac <41337764+sisazac@users.noreply.github.com>
2018-10-07 21:04:59 -0500

    Add files via upload

---
+++ laboratorios/lab03/codigo/pivot.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+/* Title: PosicionOptima
+     * Authors: Arango, Isabella and Rengifo, Juan David
+     * Date: October 7, 2018
+     * Taken from:https:https://github.com/iarangor1/ST0245-032/blob/master/laboratorios/lab03/codigo/Lab03.java
+     */
+public class pivot
+{
+    static int size;
+   public static void PosicionOptima(LinkedList<Integer>a, int data){
+        System.out.println("La lista es: " + a);
+        int count=0;
+        for(int i=0; i<=size; i++){
+            count+=a.get(i);
+        }
+        int mitad1=count/2;
+        int count2=0; 
+        int posmax=0;
+        for(int j=0; j<=size; j++){
+            count2+=a.get(j);
+            if(count2>mitad1){
+                posmax=j-1;
+                break;
+            }
+        }
+        System.out.println("El pivote debe de ir en la posicion: " + posmax);
+    }
+}

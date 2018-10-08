acaf0cd474dc9e5b4ad7b45a52993fb098141f88
sisazac <41337764+sisazac@users.noreply.github.com>
2018-10-07 19:10:56 -0500

    Add files via upload

--- laboratorios/lab03/codigo/Mall.java
+++ laboratorios/lab03/codigo/Mall.java
@@ -8,32 +8,27 @@ import java.util.*;
  */
 public class Mall
 {
-    static ArrayList<Fridge> fridges=new ArrayList<>();
-    static ArrayList<Order> orders=new ArrayList();
-    static int sizeO= orders.size();
-    static int sizeF= fridges.size();
+    static Stack<Fridge> fridges=new Stack<>();
+    static Queue<Order> orders=new LinkedList();
+    static int size= orders.size();
     public static void addFridge(Fridge fridge){
-        for(int i= sizeF-1;i>=0;i--){
-            fridges.set(i+1, fridges.get(i));
-        }
-        fridges.add(0,fridge);
+        fridges.push(fridge);
     }
 
     public static void addOrder(Order order){
-        orders.add(order);
+       orders.add(order);
     }
 
     public static void service(){
-        for(int i=0;i<sizeO;i++){
-            Order order = orders.get(i);
+        for(int i=0;i<size;i++){
+            Order order = orders.remove();
             int n=order.getNumber();
             System.out.print(order.getName() + " :");
-            while(sizeO!=0 && fridges.size()!=0){
+            while(size!=0 && fridges.size()!=0){
                 Fridge temp= fridges.pop();
                 System.out.println("The code is: "+ temp.getCode()+"and the data: "+temp.getData());
                 System.out.println("--------------------------");
                 n--;
-                
             }
         }
     }

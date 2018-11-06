f18ae7242614e34a25a2d6bb8a817366b43af90a
hsgomezo <41337724+hsgomezo@users.noreply.github.com>
2018-11-05 20:52:37 -0500

    Add files via upload

--- proyecto/codigo/Main.java
+++ proyecto/codigo/Main.java
@@ -10,12 +10,26 @@ public class Main
 {
     public static void main (String [] args){
         PrevencionColisiones colisiones= new PrevencionColisiones();
+        long startTime2 = System.currentTimeMillis();
         colisiones.areaDeUbicacion(colisiones.leerArchivo(1000000));
+        long estimatedTime2 = System.currentTimeMillis() - startTime2;
+        System.out.println("El algoritmo areaDeUbicacion tomo un tiempo de: "+estimatedTime2+" ms");
+        
         long startTime = System.currentTimeMillis();
         colisiones.detectarColisiones(colisiones.arregloDeAbejas);
         long estimatedTime = System.currentTimeMillis() - startTime;
-        System.out.println("El algoritmo tomo un tiempo de: "+estimatedTime+" ms");
-        colisiones.guardarArchivo(1000000);
+        System.out.println("El algoritmo detectarColisiones tomo un tiempo de: "+estimatedTime+" ms");
         
+        long startTime1 = System.currentTimeMillis();
+        colisiones.guardarArchivo(1000000);
+        long estimatedTime1 = System.currentTimeMillis() - startTime1;
+        System.out.println("El algoritmo guardarArchivo tomo un tiempo de: "+estimatedTime1+" ms");
+        
+        //colisiones.imprimirResultado();
+        
+        //prueba colisiones= new prueba();
+        //colisiones.tamaño(colisiones.leerArchivo(10));
+        //colisiones.detectarColisiones(colisiones.arregloDeAbejas);
+        //colisiones.imprimirResultado();
     }
 }
